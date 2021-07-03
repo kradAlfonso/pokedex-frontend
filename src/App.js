9451eb373cb78cfc9b4500bd55187f2f49bcd3dc
AlfonsoKrad <65353103+AlfonsoKrad@users.noreply.github.com>
2021-07-02 22:54:32 -0500

    Update
    Features added:
    -Search Pokemon with number and name
    -When you click the photo front to back and viceversa

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { Button, Column, Grid, Row } from "carbon-components-react";
+import { Button, Column, Grid, Row, Search} from "carbon-components-react";
 import {useState} from "react"
 import Card from "./components/card";
 
 const App = () => {
-  const [pokemonImage, setPokemonImage] = useState("");
-  const [pokemonName, setPokemonName] = useState("");
-  const [pokemonTypes, setPokemonTypes] = useState([]);
-  const [pokemonMoves, setPokemonMoves] = useState([]);
+  const [pokemonImageTemp, setPokemonImage] = useState("");
+  const [pokemonNameTemp, setPokemonName] = useState("");
+  const [pokemonTypesTemp, setPokemonTypes] = useState([]);
+  const [pokemonMovesTemp, setPokemonMoves] = useState([]);
+  const [inputName, setInputName] = useState("charizard");
+  
   const getPokemon = (pokemonName) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) => {
-        setPokemonImage(result.sprites.front_default);
-        setPokemonName(result.name);
-        setPokemonTypes(Array.from(result.types));
-        setPokemonMoves(Array.from(result.moves));
-      });
+    if(""!=pokemonName)
+    {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((result) => {
+          setPokemonImage(result.sprites);
+          setPokemonName(result.name);
+          setPokemonTypes(Array.from(result.types));
+          setPokemonMoves(Array.from(result.moves));
+        });
+    }
   };
 
   return (
@@ -25,15 +30,18 @@ const App = () => {
         <Row>
           <Column>
             <Card
-              pokemonImage = {pokemonImage} 
-              pokemonName  = {pokemonName}
-              pokemonTypes = {pokemonTypes}
-              // pokemonMoves = {pokemonMoves}
+              pokemonImage = {pokemonImageTemp} 
+              pokemonName  = {pokemonNameTemp}
+              pokemonTypes = {pokemonTypesTemp}
+              pokemonMoves = {pokemonMovesTemp}
             />
+            <Search placeholder="charizard" id="pokeSearch" onChange={() => 
+              setInputName(document.getElementById("pokeSearch").value)
+            } />
             <Button onClick={() => 
-              {
-                getPokemon("charizard");
-                console.log("Get Pokemon Charizard");
+              {  
+                getPokemon(inputName.toLowerCase());
+                console.log("Get Pokemon " + inputName);
               } 
             }>
               Load Card...
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,21 +1,31 @@
 import {ListItem, Tile, UnorderedList} from "carbon-components-react";
-import { useState } from "react";
 
 const Card = ({pokemonImage = "",pokemonName = "",pokemonTypes = [],pokemonMoves = []}) =>{
+    
+    var  pokemonImageToDisplay = pokemonImage.front_default;
+    const changeImage = () => {
+        pokemonImageToDisplay =  (pokemonImageToDisplay === pokemonImage.back_default) ? pokemonImage.front_default : pokemonImage.back_default
+        document.getElementById("pokemongImage").src= pokemonImageToDisplay
+    }
+
+    const capitalFirstLetter = (string) =>{
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+    
     return(
         <>
             <Tile>
-                <h4><strong>{pokemonName}</strong></h4>
-                <img src={pokemonImage}></img>
+                <h4><strong>{capitalFirstLetter(pokemonName)}</strong></h4>
+                <img onClick = {()=>changeImage()} src={pokemonImageToDisplay}  id="pokemongImage"></img>
                 <br></br>
-                <strong>Type:</strong>&nbsp;{pokemonTypes.map((t) => t.type.name + " ")}
+                <strong>Type:</strong>&nbsp;{pokemonTypes.map((t) => capitalFirstLetter(t.type.name) + " ")}
                 <br/>
                 <br/>
                 <strong>Moves:</strong>
                 <UnorderedList>
-                    {/* <ListItem>{pokemonMoves[0]}</ListItem>
-                    <ListItem>{pokemonMoves[1]}</ListItem>
-                    <ListItem>{pokemonMoves[2]}</ListItem> */}
+                    {pokemonMoves.slice(0,3).map(m => {
+                        return <ListItem>{capitalFirstLetter(m.move.name)}</ListItem>
+                    })}
                 </UnorderedList>
             </Tile>
         </>
